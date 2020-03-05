e92d4ce4326e2237a5099ab78ff20ff1276897e4
TSKline <klinethomas@hotmail.com>
2020-03-05 12:11:18 -0600

    Add files via upload

---
+++ Auto/index.php
@@ -0,0 +1,24 @@
+<?php $items = array("Volvo", "BMW", "Toyota", "Chrysler", "Ford");?>
+<style>
+	<?php include 'css/styles.css'; ?>
+</style>
+<html>
+	<body>
+	<?php $welcome = "Welcome to the Auto Parts Store"; ?>
+	<div class='top'>
+		<h1 id="title"><?php echo $welcome ?></h1>
+	</div>
+	<div class='mid'>
+		<ul>
+		<?php foreach($items as $i) {
+			?><li class="items">
+			<?php echo $i; ?>
+			</li>
+		<?php } ?>
+		</ul>
+	</div>
+	<div class='bot'>
+	
+	</div>
+	</body>
+</html>
